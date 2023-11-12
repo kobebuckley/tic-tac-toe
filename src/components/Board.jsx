6b94bafbe064c6eea773790bfd633a9d178d2806
kobebuckley <kobetbuckley@gmail.com>
2023-11-11 19:50:29 -1000

    created an inner board to track just the board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -27,14 +27,18 @@ const Board = () => {
       <div id='game-board' className='p-4 border-8 border-sky-500'>
 
         <h1 className="text-center text-6xl font-bold underline">Tic Tac Toe Board</h1>
+
+        <div className='p-2 m-2 items-center justify-center border-8 border-red-800' >
+
         {board.map((row, rowIndex) => (
           <div className='p-16 m-2 items-center justify-center border-8 border-gray-800' key={rowIndex}>
             {row.map((cell, colIndex) => (
-              <span className='p-16 border-4 font-bold text-4xl border-purple-700' key={colIndex}>{cell === '' ? '__' : cell}</span>
+              <span className=' p-16 m-2 items-center justify-center border-8 border-purple-800' key={colIndex}>{cell === '' ? '__' : cell}</span>
             ))}
           </div>
         ))}
       </div>
+        </div>
 
       </div>
 
