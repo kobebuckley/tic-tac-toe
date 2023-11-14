df175518ac0a4535a58a6826ec42f7fa30a398de
kobebuckley <kobetbuckley@gmail.com>
2023-11-13 21:04:53 -1000

    general board design setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 // import { useState } from "react";
-import './index.css'
+import { useState } from 'react'
+import './App.css'
 
 
 function App() {
 
+  const [boardData, setBoardData] = useState([null,null,null,null,null,null,null,null,null])
+
+  function Tile({index, tile}) {
+    return (
+      <div className="Tile">
+        {tile}{index}
+      </div>
+    )
+  }
+
+  function Board() {
+    return (
+      <div className="Board">
+        {
+          boardData.map((tile, index) => {
+            return <Tile key={index} index={index} tile={tile} />
+          })
+        }
+      </div>
+    )
+  }
+
+  
   return (
-    <div>
-      <h1>Here</h1>
+    <div className="App">
+      <Board/>
     </div>
   )
 }
