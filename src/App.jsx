56c55512cbc40f3a089e738b899e31d3d49958ea
kobebuckley <kobetbuckley@gmail.com>
2023-11-14 19:51:18 -1000

    included onClick that will update the board state if the current tile is null

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,18 @@ function App() {
 
   const [boardData, setBoardData] = useState([null,null,null,null,null,null,null,null,null])
 
+  const updateBoard = (index) => {
+    const newBoardData = [...boardData];
+    if(newBoardData[index] === null) {
+      newBoardData[index] = 'test here';
+      setBoardData(newBoardData)
+    }
+  }
+
   function Tile({index, tile}) {
     return (
-      <div className="Tile">
-        {tile}{index}
+      <div className="Tile" onClick={() => updateBoard(index)}>
+        {tile}
       </div>
     )
   }
